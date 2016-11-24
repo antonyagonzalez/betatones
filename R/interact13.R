be498bfdadc1e30427d2c0483e0c666291d60bc8
antonyagonzalez <agonzalez@psych.ubc.ca>
2016-11-24 13:21:59 -0800

    interact13()

---
+++ R/interact13.R
@@ -0,0 +1,23 @@
+#' Extract regression model data for 2 way interaction (predictor 1 & 3)
+#'
+#' Pull the regression coefficient, standard error, t-value and p-value
+#' for the two way interaction between the first and third predictors
+#' specified in your linear model. If you have a covariate in your model,
+#' use cov_interact13 (for 2 covariates, use cov2_interact13).
+#'
+#' @param lm linear model
+#'
+#' @return beta, se, t-value, p-value
+#' @export
+#' @examples
+#' example <- lm(formula = Sepal.Length ~ Petal.Length * Petal.Width * Sepal.Width, data = iris)
+#' interact13(example)
+interact13 <- function(lm) {
+  modelsum <- summary(lm)
+  out <- c(modelsum$coefficients[6],
+           modelsum$coefficients[6,2],
+           modelsum$coefficients[6,3],
+           modelsum$coefficients[6,4])
+  names(out) <- c("beta", "se", "t-value", "p-value")
+  return(out)
+}
