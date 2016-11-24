3dbc37c676571786aa9ba871eb48e5f233eb0fde
antonyagonzalez <agonzalez@psych.ubc.ca>
2016-11-24 12:44:06 -0800

    Initial commit

---
+++ R/predictor1.R
@@ -0,0 +1,9 @@
+predictor1 <- function(lm) {
+  modelsum <- summary(lm)
+  out <- c(modelsum$coefficients[2],
+           modelsum$coefficients[2,2],
+           modelsum$coefficients[2,3],
+           modelsum$coefficients[2,4])
+  names(out) <- c("beta", "se", "t-value", "p-value")
+  return(out)
+}
